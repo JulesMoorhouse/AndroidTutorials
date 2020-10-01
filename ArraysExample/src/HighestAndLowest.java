62bd40d87106185d9bf45f5877585badbaaf36a0
Jules Moorhouse <julesthecoder@gmail.com>
2020-10-01 16:17:55 +0100

    Highest / lowest / sum and average

--- ArraysExample/src/HighestAndLowest.java
+++ ArraysExample/src/HighestAndLowest.java
@@ -2,6 +2,30 @@ public class HighestAndLowest
 {
     public static void main(String[] args)
     {
+        int [] numbers = {2 ,-1 ,35, 12, 15, 17, 1};
 
+        int highest = numbers[0];
+
+        for (int i = 1; i < numbers.length; i++)
+        {
+            if (numbers[i] > highest)
+            {
+                highest = numbers[i];
+            }
+        }
+
+        System.out.println("The highest value is: " + highest);
+
+        int lowest = numbers[0];
+
+        for (int i = 1; i < numbers.length; i++)
+        {
+            if (numbers[i] < lowest)
+            {
+                lowest = numbers[i];
+            }
+        }
+
+        System.out.println("The lowest value is: " + lowest);
     }
 }
