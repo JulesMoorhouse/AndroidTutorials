0d277ea674e782a03c7d1b2657a36ef00a0ee8ea
Jules Moorhouse <julesthecoder@gmail.com>
2020-10-01 16:40:32 +0100

    String arrays

--- ArraysExample/src/StringArrays.java
+++ ArraysExample/src/StringArrays.java
@@ -6,6 +6,27 @@ public class StringArrays
 {
     public static void main(String[] args) throws IOException
     {
+        String [] names = getNames();
 
+        for (int i = 0; i < names.length; i++)
+        {
+            if (names[i] != null)
+            {
+                System.out.println(names[i].charAt(0));
+            }
+        }
+    }
+
+    public static String [] getNames()
+    {
+        String [] names = new String[6];
+
+        names[0] = "John";
+        names[1] = "Peter";
+        names[2] = "Paul";
+        names[3] = "Simon";
+        names[4] = "Chuck";
+
+        return names;
     }
 }
