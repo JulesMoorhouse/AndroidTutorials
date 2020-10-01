0f68594fc31697c6ce17bd4263362eea3dfa4bdc
Jules Moorhouse <julesthecoder@gmail.com>
2020-10-01 16:04:59 +0100

    New classes

---
+++ ArraysExample/src/HighestAndLowest.java
@@ -0,0 +1,7 @@
+public class HighestAndLowest
+{
+    public static void main(String[] args)
+    {
+
+    }
+}
---
+++ ArraysExample/src/SumAndAverage.java
@@ -0,0 +1,7 @@
+public class SumAndAverage
+{
+    public static void main(String[] args)
+    {
+
+    }
+}
