aa2e28d6e54a989e8804f20f2d47a9323a03612f
Jules Moorhouse <julesthecoder@gmail.com>
2020-10-01 13:43:59 +0100

    Reading and writing files.

--- WorkingWithFiles/src/com/example/textfiles/WritingToFiles.java
+++ WorkingWithFiles/src/com/example/textfiles/WritingToFiles.java
@@ -1,9 +1,21 @@
 package com.example.textfiles;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class WritingToFiles
 {
-    public static void main(String[] args)
+    public static void main(String[] args) throws IOException
     {
+        // Create the file and open the file
+        // If the file exists it will replace the file
+        PrintWriter outputFile = new PrintWriter("OutputFile.txt");
 
+        outputFile.println("This in line 1");
+        outputFile.println("This in line 2");
+        outputFile.println("This in line 3");
+        outputFile.println("This in line 4");
+
+        outputFile.close();
     }
 }
