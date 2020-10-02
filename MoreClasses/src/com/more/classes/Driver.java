8143c44fc582f34c562fd19a964c4ce9aece5cc0
Jules Moorhouse <julesthecoder@gmail.com>
2020-10-02 09:28:32 +0100

    More classes and objects

--- MoreClasses/src/com/more/classes/Driver.java
+++ MoreClasses/src/com/more/classes/Driver.java
@@ -2,4 +2,25 @@ package com.more.classes;
 
 public class Driver
 {
+    public static void main(String[] args)
+    {
+        Subject subject1 = new Subject("TPB10AB", "Technical Programming 1");
+//        Subject subject2 = new Subject("TPB10AB", "Technical Programming 1");
+//
+//        if (subject1.equals(subject2))
+//        {
+//            System.out.println("They are the same!");
+//        }
+//
+//        System.out.println(subject1);
+
+        Student student1 = new Student("Chuck Norris", 1232435325);
+
+        ExamPaper paper = new ExamPaper(student1, subject1, 100);
+
+        //System.out.println(paper);
+
+        System.out.println("Subject code: " + paper.getSubject().getSubjectCode());
+        System.out.println("Student number: " + paper.getStudent().getStudentNumber());
+    }
 }
--- MoreClasses/src/com/more/classes/ExamPaper.java
+++ MoreClasses/src/com/more/classes/ExamPaper.java
@@ -1,4 +1,57 @@
 package com.more.classes;
 
-public class ExamPaper {
+public class ExamPaper
+{
+    private Student student;
+    private Subject subject;
+    private int marks;
+
+    public ExamPaper(Student student, Subject subject, int marks)
+    {
+        // Setting references to same objects student and subject!!
+        //this.student = student;
+        //this.subject = subject;
+
+        this.student = new Student(student);
+        this.subject = new Subject(subject);
+        this.marks = marks;
+    }
+
+    public Student getStudent()
+    {
+        return new Student(student);
+    }
+
+    public void setStudent(Student student)
+    {
+        this.student = new Student(student);
+    }
+
+    public Subject getSubject()
+    {
+        return new Subject(subject);
+    }
+
+    public void setSubject(Subject subject)
+    {
+        this.subject = new Subject(subject);
+    }
+
+    public int getMarks()
+    {
+        return marks;
+    }
+
+    public void setMarks(int marks)
+    {
+        this.marks = marks;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Exam paper information:\n" +
+                student + "\n" + subject + "\n" +
+                "Marks : " + marks;
+    }
 }
