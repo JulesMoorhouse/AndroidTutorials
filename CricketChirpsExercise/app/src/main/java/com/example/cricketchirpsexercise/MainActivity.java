bf5bcbec9c92f88075a34633c275f0113f9ba5ca
Jules Moorhouse <julesthecoder@gmail.com>
2020-10-06 14:54:58 +0100

    Completed exercise

---
+++ CricketChirpsExercise/app/src/main/java/com/example/cricketchirpsexercise/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.cricketchirpsexercise;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity
+{
+    TextView lblCalculation;
+    EditText txtChirps;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        lblCalculation = (TextView)findViewById(R.id.lblCalculation);
+        lblCalculation.setVisibility(View.GONE);
+        txtChirps = (EditText)findViewById(R.id.txtChirps);
+    }
+
+    public void calcTemp(View view)
+    {
+        int chirps = Integer.parseInt(txtChirps.getText().toString());
+        int temp = (chirps / 3) + 4;
+        String sentence = getString(R.string.lblApproxTemp, temp);
+        lblCalculation.setText(sentence);
+        lblCalculation.setVisibility(View.VISIBLE);
+    }
+}
