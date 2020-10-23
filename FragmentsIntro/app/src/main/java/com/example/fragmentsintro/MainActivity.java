6328c8cb8f6fb4924219cc94748a913e9d0453b6
Jules Moorhouse <julesthecoder@gmail.com>
2020-10-23 14:57:51 +0100

    Completed FragmentsIntro

--- FragmentsIntro/app/src/main/java/com/example/fragmentsintro/MainActivity.java
+++ FragmentsIntro/app/src/main/java/com/example/fragmentsintro/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.fragmentsintro;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 import android.widget.TextView;
@@ -10,7 +11,7 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity implements ListFrag.ItemSelected {
 
     TextView tvDescription;
-    ArrayList<String> descriptions;
+    String [] descriptions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -20,17 +21,46 @@ public class MainActivity extends AppCompatActivity implements ListFrag.ItemSele
 
         tvDescription = findViewById(R.id.tvDescription);
 
-        descriptions = new ArrayList<String>();
-        descriptions.add("Description for item 1");
-        descriptions.add("Description for item 2");
-        descriptions.add("Description for item 3");
+        descriptions = getResources().getStringArray(R.array.descriptions);
 
+        //the phone is in portrait mode
+        if (findViewById(R.id.layout_portrait) != null)
+        {
+            FragmentManager manager = this.getSupportFragmentManager();
 
+            manager.beginTransaction()
+                    .hide(manager.findFragmentById(R.id.detailFrag))
+                    .show(manager.findFragmentById(R.id.listFrag))
+                    .commit();
+        }
+
+        //the phone is in landscape mode
+        if (findViewById(R.id.layout_land) != null)
+        {
+            FragmentManager manager = this.getSupportFragmentManager();
+
+            manager.beginTransaction()
+                    .show(manager.findFragmentById(R.id.detailFrag))
+                    .show(manager.findFragmentById(R.id.listFrag))
+                    .commit();
+        }
     }
 
     @Override
     public void onItemSelected(int index)
     {
-        tvDescription.setText(descriptions.get(index));
+        tvDescription.setText(descriptions[index]);
+
+        //the phone is in portrait mode
+        if (findViewById(R.id.layout_portrait) != null)
+        {
+            FragmentManager manager = this.getSupportFragmentManager();
+
+            manager.beginTransaction()
+                    .show(manager.findFragmentById(R.id.detailFrag))
+                    .hide(manager.findFragmentById(R.id.listFrag))
+                    .addToBackStack(null)
+                    .commit();
+        }
     }
 }
