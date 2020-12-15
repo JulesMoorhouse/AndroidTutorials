211a76ac0b23a2dde2c87de5293cbc7e97e4265d
Jules Moorhouse <julesthecoder@gmail.com>
2020-12-15 11:22:31 +0000

    ActionBar example

---
+++ ActionBar/app/src/main/java/com/example/actionbar/Main2Activity.java
@@ -0,0 +1,23 @@
+package com.example.actionbar;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setIcon(R.drawable.chrome);
+        actionBar.setTitle(" Second Activity");
+        actionBar.setDisplayUseLogoEnabled(true);
+        actionBar.setDisplayShowHomeEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+    }
+}
--- ActionBar/app/src/main/java/com/example/actionbar/MainActivity.java
+++ ActionBar/app/src/main/java/com/example/actionbar/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.actionbar;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -12,5 +18,41 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setIcon(R.drawable.chrome);
+        actionBar.setTitle(" Welcome");
+        actionBar.setDisplayUseLogoEnabled(true);
+        actionBar.setDisplayShowHomeEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.main, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId())
+        {
+            case R.id.download:
+                Toast.makeText(this, "Download Clicked!", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(this, Main2Activity.class));
+                break;
+
+            case R.id.send:
+                Toast.makeText(this, "Send Clicked!", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.refresh:
+                Toast.makeText(this, "Refresh Clicked!", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
