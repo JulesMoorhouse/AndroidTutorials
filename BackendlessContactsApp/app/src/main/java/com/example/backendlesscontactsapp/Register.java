b5d1eab9b918f1b9e9006819ed60407d74b9fc70
Jules Moorhouse <julesthecoder@gmail.com>
2021-01-20 10:32:25 +0000

    Registration and Backendless changes.:

--- BackendlessContactsApp/app/src/main/java/com/example/backendlesscontactsapp/Register.java
+++ BackendlessContactsApp/app/src/main/java/com/example/backendlesscontactsapp/Register.java
@@ -7,6 +7,7 @@ import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -79,6 +80,8 @@ public class Register extends AppCompatActivity {
 
                             @Override
                             public void handleFault(BackendlessFault fault) {
+
+                                Log.d("Backendless.UserService", fault.getMessage());
 
                                 Toast.makeText(Register.this, "Error: " + fault.getMessage(),
                                         Toast.LENGTH_SHORT).show();
