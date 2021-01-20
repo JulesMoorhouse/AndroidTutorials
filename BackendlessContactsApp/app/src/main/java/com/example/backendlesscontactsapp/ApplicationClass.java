03c368fed5822603d7cc84390f8959fa3eacabbe
Jules Moorhouse <julesthecoder@gmail.com>
2021-01-20 11:19:25 +0000

    Server url fix.

--- BackendlessContactsApp/app/src/main/java/com/example/backendlesscontactsapp/ApplicationClass.java
+++ BackendlessContactsApp/app/src/main/java/com/example/backendlesscontactsapp/ApplicationClass.java
@@ -8,7 +8,7 @@ public class ApplicationClass extends Application
 {
     public static final String APPLICATION_ID = "";
     public static final String API_KEY = "";
-    public static final String SERVER_URL = "https://api.backendless.com";
+    public static final String SERVER_URL = "https://eu-api.backendless.com";
 
     @Override
     public void onCreate() {
